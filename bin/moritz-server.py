c24d7f64c6e3a5d53230f77944759e3b7bd81ef8
Markus Ullmann <mail@markus-ullmann.de>
2014-12-08 03:17:00 +0100

    Convert application to global object as well as its views

--- bin/moritz-server.py
+++ bin/moritz-server.py
@@ -27,14 +27,10 @@ from moritzprotocol.messages import SetTemperatureMessage
 
 # local constantsfrom datetime import datetime
 encoder.FLOAT_REPR = lambda o: format(o, '.2f')
-THERMOSTATS = {
-    0x0E023B: "Bad",
-    0x0E04A6: "Schlafzimmer",
-    0x0E016C: "Wohnzimmer",
-}
-THERMOSTATS_BY_NAME = dict((v,k) for k, v in THERMOSTATS.items())
 
-
+#
+# Environment Setup
+#
 class JSONWithDateEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, datetime):
@@ -46,26 +42,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/moritz-server.db'
 db = SQLAlchemy(app)
 
-def main(args):
-    command_queue = Queue.Queue()
-    message_thread = CULMessageThread(command_queue, args.cul_path)
-    message_thread.start()
+command_queue = Queue.Queue()
 
-
-    @app.route("/")
-    def index():
-        with message_thread.thermostat_states_lock:
-            return json.dumps(message_thread.thermostat_states, cls=JSONWithDateEncoder)
-
-    @app.route("/set_temp", methods=["GET", "POST"])
-    def set_temp():
-        if not request.form:
-            content = """<html><form action="" method="POST"><select name="thermostat">"""
-            for thermo_id, thermo_name in THERMOSTATS.items():
-                content += """<option value="%s">%s</option>""" % (thermo_id, thermo_name)
-            content += """</select><select name="mode"><option>auto</option><option selected>manual</option><option>boost</option></select>"""
-            content += """<input type=text name=temperature><input type=submit value="set"></form></html>"""
-            return content
 #
 # Models
 #
@@ -103,36 +81,61 @@ class ThermostatState(db.Model):
     battery_low = db.Column(db.Boolean, nullable=True)
 
 
+#
+# Views
+#
+@app.route("/")
+def index():
+    with message_thread.thermostat_states_lock:
+        return json.dumps(message_thread.thermostat_states, cls=JSONWithDateEncoder)
+
+@app.route("/set_temp", methods=["GET", "POST"])
+def set_temp():
+    if not request.form:
+        content = """<html><form action="" method="POST"><select name="thermostat">"""
+        for thermostat in Thermostat.query.filter_by(paired=True):
+            content += """<option value="%s">%s</option>""" % (thermostat.sender_id, thermostat.name)
+        content += """</select><select name="mode"><option>auto</option><option selected>manual</option><option>boost</option></select>"""
+        content += """<input type=text name=temperature><input type=submit value="set"></form></html>"""
+        return content
+    msg = SetTemperatureMessage()
+    msg.counter = 0xB9
+    msg.sender_id = CUBE_ID
+    msg.receiver_id = int(request.form['thermostat'])
+    msg.group_id = 0
+    payload = {
+        'desired_temperature': float(request.form["temperature"]),
+        'mode': request.form["mode"],
+    }
+    command_queue.put((msg, payload))
+    return """<html>Done. <a href="/">back</a>"""
+
+@app.route("/set_temp_all", methods=["GET", "POST"])
+def set_temp_all():
+    if not request.form:
+        content = """<html><form action="" method="POST"><select name="mode"><option>auto</option><option selected>manual</option><option>boost</option></select>"""
+        content += """<input type=text name=temperature><input type=submit value="set"></form></html>"""
+        return content
+    for thermostat in Thermostat.query.filter_by(paired=True):
         msg = SetTemperatureMessage()
         msg.counter = 0xB9
         msg.sender_id = CUBE_ID
-        msg.receiver_id = int(request.form['thermostat'])
+        msg.receiver_id = thermostat.sender_id
         msg.group_id = 0
         payload = {
             'desired_temperature': float(request.form["temperature"]),
             'mode': request.form["mode"],
         }
         command_queue.put((msg, payload))
-        return """<html>Done. <a href="/">back</a>"""
+    return """<html>Done. <a href="/">back</a>"""
 
-    @app.route("/set_temp_all", methods=["GET", "POST"])
-    def set_temp_all():
-        if not request.form:
-            content = """<html><form action="" method="POST"><select name="mode"><option>auto</option><option selected>manual</option><option>boost</option></select>"""
-            content += """<input type=text name=temperature><input type=submit value="set"></form></html>"""
-            return content
-        for thermo_id in THERMOSTATS:
-            msg = SetTemperatureMessage()
-            msg.counter = 0xB9
-            msg.sender_id = CUBE_ID
-            msg.receiver_id = thermo_id
-            msg.group_id = 0
-            payload = {
-                'desired_temperature': float(request.form["temperature"]),
-                'mode': request.form["mode"],
-            }
-            command_queue.put((msg, payload))
-        return """<html>Done. <a href="/">back</a>"""
+#
+# Execution
+#
+def main(args):
+    global message_thread
+    message_thread = CULMessageThread(command_queue, args.cul_path)
+    message_thread.start()
 
     if args.flask_debug:
         app.run(host="0.0.0.0", port=12345, debug=True, use_reloader=False)
